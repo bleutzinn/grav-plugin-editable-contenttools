3b5282e78ea28595177d5dfcb98eae57b399f97b
bleutzinn <bleutzinn@rwgc.nl>
2019-02-02 22:39:39 +0100

    Removed unneeded library references

--- shortcodes/EditableContentToolsShortcode.php
+++ shortcodes/EditableContentToolsShortcode.php
@@ -2,44 +2,33 @@
 
 namespace Grav\Plugin\Shortcodes;
 
-use Grav\Common\Grav;
-use Grav\Common\Plugin;
-use Grav\Common\Page\Page;
-use Grav\Common\Uri;
-use Grav\Plugin\EditableContentToolsPlugin;
 use Thunder\Shortcode\Shortcode\ShortcodeInterface;
 
 
 /**
- * Inserts content from the editable content page and makes it editable for the editor.
+ * Wraps content of an editable shortcode to turn it
+ * into an editable region for ContentTools.
  */
 class EditableContentToolsShortcode extends Shortcode
 {
     public function init()
     {
         $this->shortcode->getHandlers()->add('editable', function(ShortcodeInterface $sc) {
+            
+            // Get variables to be used in the template
             $sc_name = $sc->getParameter('name', '');
-            /*
-            if (preg_match('/example-[0-9]/', $sc_name, $matches)) {
-                return EditableContentToolsPlugin::showShortcodeExamples($matches[0]);
-            }
-            else {
-            */
-                // Get variables to be used in the template
-                $page = $this->grav['page'];
-                $route = $page->route();
-                $content = $sc->getContent();
-                // Render the template
-                $output = $this->twig->processTemplate('editable-region.html.twig', [
-                    'name' => $sc_name,
-                    'route' => $route,
-                    'content' => $content,
-                ]);
-                // Return the rendered output (HTML)
-                return $output;
-            /*
-            }
-            */
+            $content = $sc->getContent();
+            $route = $this->grav['page']->route();
+
+            // Render the template
+            $output = $this->twig->processTemplate('editable-region.html.twig', [
+                'name' => $sc_name,
+                'content' => $content,
+                'route' => $route,
+            ]);
+
+            // Return the rendered output (HTML)
+            return $output;
         });
     }
 }
