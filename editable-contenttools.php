5b648fc9e2582881d97eb2be917fcb1ac13290af
bleutzinn <bleutzinn@rwgc.nl>
2019-02-02 22:25:37 +0100

    Updated to version 1.0

--- editable-contenttools.php
+++ editable-contenttools.php
@@ -3,8 +3,8 @@ namespace Grav\Plugin;
 
 use Grav\Common\Page\Page;
 use Grav\Common\Plugin;
-use Grav\Common\Utils;
 use Grav\Common\Twig\Twig;
+use Grav\Common\Utils;
 use RocketTheme\Toolbox\Event\Event;
 
 /**
@@ -16,7 +16,7 @@ class EditableContentToolsPlugin extends Plugin
 
     protected $my_name = 'editable-contenttools';
     protected $my_full_name = 'Editable ContentTools';
-    public $sc_ids = [];
+    protected $token = 'editable-contenttools-api';
 
     /**
      * Add Editor code and styles
@@ -26,20 +26,22 @@ class EditableContentToolsPlugin extends Plugin
         // Get assets objects
         $assets = $this->grav['assets'];
 
-        if ($this->user_authorized) {
-            // Add editor assets
-            // Add styles
-            $assets->addCss('plugin://' . $this->my_name . '/vendor/content-tools.min.css', 1);
-            $assets->addCss('plugin://' . $this->my_name . '/css/editor.css', 1);
-            // Add code
-            //$assets->AddJs('https://unpkg.com/turndown/dist/turndown.js', 1);
-            $assets->addJs('plugin://' . $this->my_name . '/vendor/turndown.js', 1);
-            $assets->AddJs('https://unpkg.com/turndown-plugin-gfm/dist/turndown-plugin-gfm.js', 1);
-            $assets->addJs('plugin://' . $this->my_name . '/vendor/content-tools.min.js', 1);
-            // Add reference to dynamically created initialisation code
-            // (to be created in $this->onPagesInitialised())
-            $assets->addJs($this->my_name . $this->grav['page']->route() . '/editor.js', ['group' => 'bottom']);
+        // Add styles
+        $assets->addCss('plugin://' . $this->my_name . '/vendor/content-tools.min.css', 1);
+        $assets->addCss('plugin://' . $this->my_name . '/css/editor.css', 1);
+
+        // Add code
+        $assets->addJs('plugin://' . $this->my_name . '/vendor/turndown.js', 1);
+        $assets->AddJs('https://unpkg.com/turndown-plugin-gfm/dist/turndown-plugin-gfm.js', 1);
+        $assets->addJs('plugin://' . $this->my_name . '/vendor/content-tools.min.js', 1);
+
+        // Add reference to dynamically created assets
+        $route = $this->grav['page']->route();
+        if ($route == '/') {
+            $route = '';
         }
+        //$assets->addJs($this->my_name . $route . '/editor.js', ['group' => 'bottom']);
+        $assets->addJs($this->my_name . '-api' . $route . '/editor.js', ['group' => 'bottom']);
     }
 
     /**
@@ -55,41 +57,8 @@ class EditableContentToolsPlugin extends Plugin
     public static function getSubscribedEvents()
     {
         return [
-            'onPluginsInitialized' => ['onPluginsInitialized', 0]
+            'onPluginsInitialized' => ['onPluginsInitialized', 0],
         ];
-    }
-
-    /**
-     * Handle resource requests
-     * 
-     * @return mixed
-     */
-    public function handleResource($page)
-    {
-        $paths = $this->grav['uri']->paths();
-        // Check whether the requested page name equals this plugin name
-        if (array_shift($paths) == $this->my_name) {
-            $asset = array_pop($paths);
-            $slug = array_pop($paths);
-            $route = implode('/', $paths);
-            $target_page = $this->grav['page']->find($route . '/' . $slug);
-            switch ($asset) {
-                case 'editor.js': // Return editor instantiation as Javascript
-                    $nonce = Utils::getNonce($this->my_name . '-nonce');
-                    // Render the template
-                    $output = $this->grav['twig']->processTemplate('editor.js.twig', [
-                        'save_url' => ($this->my_name . $target_page->route() . '/save'),
-                        'nonce' => $nonce
-                    ]);
-                    $this->setHeaders('text/javascript');
-                    echo $output;
-                    exit;
-                case 'save':
-                    $this->saveChanges();
-                default:
-                    return;
-            }
-        }
     }
 
     /**
@@ -103,18 +72,7 @@ class EditableContentToolsPlugin extends Plugin
             return;
         }
 
-        $page = $this->grav['page'];
-        
-        // Change this normal! page into an editable one when user is permitted
-        if ($page->modular()) {
-            $this->grav['log']->warning($this->my_full_name . ': can\'t act on modular pages');
-            $this->grav['debugger']->addMessage($this->my_full_name . ' can\'t act on modular pages');
-            return;
-        }
-        else {
-            $config = $this->mergeConfig($page);
-            $user = $this->grav['user'];
-            $username = $user->get('username');
+        if ($this->userAuthorized()) {
             $this->addAssets();
         }
     }
@@ -132,18 +90,36 @@ class EditableContentToolsPlugin extends Plugin
             return;
         }
 
-        if (!$this->user_authorized) {
-            return;
-        }
+        $paths = $this->grav['uri']->paths();
+        
+        if (array_shift($paths) == $this->token) {
+            $target = array_pop($paths);
+            $route = implode('/', $paths);
+            
+            switch ($target) {
 
-        $page = $this->grav['page'];
-        if (!$page->exists) {
-            $this->handleResource($page);
-        }
-        else {
-            if ($page->modular()) {
-                $this->grav['log']->warning($my_full_name . ' can\'t act on modular pages');
-                return;
+                case 'editor.js': // Return editor instantiation as Javascript
+                    $nonce = Utils::getNonce($this->my_name . '-nonce');
+
+                    // Create absolute URL including token and action
+                    $save_url = $this->grav['uri']->rootUrl(true) . '/' . $this->token . '/' . $route . '/save';
+                    // Render the template
+                    $output = $this->grav['twig']->processTemplate('editor.js.twig', [
+                        'save_url' => $save_url,
+                        'nonce' => $nonce,
+                    ]);
+
+                    $this->setHeaders('text/javascript');
+                    echo $output;
+                    exit;
+
+                case 'save':
+                    if ($_POST) {
+                        $this->saveRegions('/' . $route);
+                    }
+
+                default:
+                    return;
             }
         }
     }
@@ -158,20 +134,12 @@ class EditableContentToolsPlugin extends Plugin
             return;
         }
 
-        // Check for user authorisation
-        $this->user_authorized = $this->userIsAuthorized($this->grav['page'], $this->grav['user']);
-
-        // Abort when the user is not authorized
-        if (!$this->user_authorized) {
-            return;
-        }
-
         // Enable the events we are interested in
         $this->enable([
             'onPageInitialized' => ['onPageInitialized', 0],
             'onPagesInitialized' => ['onPagesInitialized', 0],
             'onShortcodeHandlers' => ['onShortcodeHandlers', 0],
-            'onTwigTemplatePaths' => ['onTwigTemplatePaths', 0]
+            'onTwigTemplatePaths' => ['onTwigTemplatePaths', 0],
         ]);
     }
 
@@ -180,7 +148,7 @@ class EditableContentToolsPlugin extends Plugin
      */
     public function onShortcodeHandlers()
     {
-        $this->grav['shortcode']->registerShortcode('EditableContentToolsShortcode.php' , __DIR__.'/shortcodes/');
+        $this->grav['shortcode']->registerShortcode('EditableContentToolsShortcode.php', __DIR__ . '/shortcodes/');
     }
 
     /**
@@ -195,52 +163,48 @@ class EditableContentToolsPlugin extends Plugin
     /**
      * Save each region content to it's corresponding shortcode
      */
-    public function saveChanges()
+    public function saveRegions($route)
     {
         $result = false;
         $post = $_POST;
-        // Do not act upon empty POST payload
-        if ($post) {
-            $nonce = $post['ct-nonce'];
-            if (Utils::verifyNonce($nonce, $this->my_name . '-nonce')) {
-                $paths = $this->grav['uri']->paths();
-                // Check whether the requested page name equals this plugin name
-                if (array_shift($paths) == $this->my_name) {
-                    $asset = array_pop($paths);
-                    $slug = array_pop($paths);
-                    $route = implode('/', $paths);
-                    $page = $this->grav['page']->find($route . '/' . $slug);
-                    $content = $page->rawMarkdown();
-                    array_shift($post);
-                    foreach($post as $key => $value){
-                        // Wrap Markdown in newlines (important!)
-                        $value = PHP_EOL . $value . PHP_EOL;
-                        preg_match('/\[editable .*?name=[\'"]' . $key . '[\'"].*?\](.*?)\[\/editable\]/is', $content, $matches);
-                        $content = str_replace($matches[1], $value, $content);
-                    }
-                    $page->rawMarkdown($content);
-                    // Do the actual save action
-                    $page->save();
-                    exit;
+        $nonce = $post['ct-nonce'];
+
+        if (Utils::verifyNonce($nonce, $this->my_name . '-nonce')) {
+            $page = $this->grav['pages']->find($route);
+            $content = $page->rawMarkdown();
+
+            foreach ($post as $key => $value) {
+
+                // Wrap Markdown in newlines (important!)
+                $value = PHP_EOL . $value . PHP_EOL;
+
+                // Replace each shortcode content
+                if (preg_match('/\[editable .*?name=[\'"]' . $key . '[\'"].*?\](.*?)\[\/editable\]/is', $content, $matches) == 1) {
+                    $content = str_replace($matches[1], $value, $content);
                 }
             }
+
+            // Do the actual save action
+            $page->rawMarkdown($content);
+            $page->save();
+            exit;
         }
-        // Saving failed        
-        if (!$result) {
-            // Create a custom error page
-            // BTW the HTTP status code is set in that page frontmatter
-            $page = new Page;
-            $page->init(new \SplFileInfo(__DIR__ . '/pages/save-error.md'));
-            // Let Grav return the error page
-            unset($this->grav['page']);
-            $this->grav['page'] = $page;
-        }
-        
+
+        // Saving failed
+        // Create a custom error page
+        // BTW the HTTP status code is set via the page frontmatter
+        $page = new Page;
+        $page->init(new \SplFileInfo(__DIR__ . '/pages/save-error.md'));
+
+        // Let Grav return the error page
+        unset($this->grav['page']);
+        $this->grav['page'] = $page;
+
     }
 
     /**
      * Set return header
-     * 
+     *
      * @return header
      */
     public function setHeaders($type = 'application/json')
@@ -252,7 +216,7 @@ class EditableContentToolsPlugin extends Plugin
         if ($expires > 0) {
             $expires_date = gmdate('D, d M Y H:i:s', time() + $expires) . ' GMT';
             header('Cache-Control: max-age=' . $expires);
-            header('Expires: '. $expires_date);
+            header('Expires: ' . $expires_date);
         }
     }
 
@@ -261,65 +225,15 @@ class EditableContentToolsPlugin extends Plugin
      *
      * @return boolean
      */
-    public function userIsAuthorized($page, $user)
+    public function userAuthorized()
     {
         $result = false;
-        if ($page) {
-            $config = $this->mergeConfig($page);
-            $username = $this->grav['user']->get('username');
-            $header = (array)$page->header();
-            // Get the groups this user is a member of
-            $groups = $this->grav['user']->get('groups');
-            // Check whether only certain users or groups may edit this page
-            $editable_by = $config->get('editable_by');
+        $user = $this->grav['user'];
 
-            if (isset($editable_by)) {
-                //$editable_by = $config->get('editable_by');
-                for ($i=0; $i < count($editable_by); $i++) {
-                    //dump($editable_by[$i]);
-                    if (is_array($editable_by[$i])) { // Array encountered
-                        foreach ($editable_by[$i] as $key => $value) {
-                            // Examine each element and check for a groups variable
-                            if ($key == 'groups') {
-                                // Match the groups specified in the editable_by variable
-                                // to the groups this user belongs to
-                                $match = array_intersect($value, $groups);
-                                if (!empty($match)) { // Bingo! There's a match
-                                    $result = true;
-                                    break; // Break from the foreach loop
-                                }
-                            }
-                            else {
-                                if ($key == 'users') {
-                                    // Find the logged in user in the list of users
-                                    $match = array_search($username, $value);
-                                    
-                                    if ($match != false) { // Bingo! There's a match
-                                        $result = true;
-                                        break; // Break from the foreach loop
-                                    }
-                                }
-                            }
-                        }
-                    }
-                    else { // Must be a username
-                        if ($editable_by[$i] == $username) { // Bingo ! A username match
-                            $result = true;
-                            break; // Break from the for loop
-                        }
-                    }
-                }
-                return $result;
-            }
-        }
-        else {
-            // No specific users or groups are specified in page frontmatter
-            // so grant access to any logged in user with proper permissions
-            $result = $this->grav['user']->authorize('site.editable') || $this->grav['user']->authorize('admin.super') || $this->grav['user']->authorize('admin.pages');
+        if ($user->authorized) {
+            $result = $user->authorize('site.editable') || $user->authorize('admin.super') || $user->authorize('admin.pages');
         }
         return $result;
     }
-
-
 
 }
